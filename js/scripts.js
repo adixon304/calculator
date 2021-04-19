77424b3e23398d3f3306c3f9e0d0a778c602740f
Alex Dixon <alexdixon15@gmail.com>
2021-04-19 10:35:11 -0400

    Base functionality of calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+let number1 = 0
+let number2 = 0
+let operator
+
 function add(number1, number2){
     return number1 + number2;
 }
@@ -29,4 +33,31 @@ function operate(operator, number1, number2){
             return divide(number1, number2);
             break;
     }
+}
+
+function setNumber(numberValue) {
+    document.getElementById('display').value = document.getElementById('display').value + numberValue;
+}
+
+function clearValues() {
+    number1 = 0;
+    number2 = 0;
+    operator = '';
+    clearDisplay();
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function setOperator(operatorValue) {
+    number1 = Number(document.getElementById('display').value);
+    operator = operatorValue;
+    clearDisplay();
+}
+
+function equals() {
+    number2 = Number(document.getElementById('display').value);
+    let calculation = operate(operator, number1, number2);
+    document.getElementById('display').value = calculation;
 }
